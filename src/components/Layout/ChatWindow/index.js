6cd7a6cbfc6dbcdf898267e1fc2f4ae8d448b1eb
Alberto Rocha <albsrocha@gmail.com>
2021-09-09 17:03:11 -0300

    Adicionando emojis

--- src/components/Layout/ChatWindow/index.js
+++ src/components/Layout/ChatWindow/index.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./index.css";
 
+import Picker from "emoji-picker-react";
+import MicIcon from "@material-ui/icons/Mic";
+import SendIcon from "@material-ui/icons/Send";
+import CloseIcon from "@material-ui/icons/Close";
 import SearchIcon from "@material-ui/icons/Search";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import AttachFileIcon from "@material-ui/icons/AttachFile";
 import IconButton from "@material-ui/core/IconButton";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 
 export default function ChatWindow({ text }) {
+
+  const handleEmojiClick = () => {
+
+  }
+
   return (
     <div className="chatWindow">
       <div className="chatWindow--header">
@@ -32,7 +42,44 @@ export default function ChatWindow({ text }) {
 
       <div className="chatWindow--body">{text}</div>
 
-      <div className="chatWindow--footer"></div>
+      <div className="chatWindow--emojiarea">
+        <Picker
+        className="emoji-picker-react" 
+        onEmojiClick={handleEmojiClick}
+        />
+      </div>
+
+      <div className="chatWindow--footer">
+        <div className="chatWindow--pre">
+          <div className="chatWindow--headerbtn">
+          <IconButton>
+              <CloseIcon />
+            </IconButton>
+
+            <IconButton>
+              <InsertEmoticonIcon />
+            </IconButton>
+
+            <IconButton>
+              <AttachFileIcon />
+            </IconButton>
+          </div>
+        </div>
+        <div className="chatWindow--input">
+          <input
+            className="input--chat"
+            type="text"
+            placeholder="Digite uma mensagem!"
+          />
+        </div>
+        <div className="chatWindow--pos">
+          <div className="chatWindow--headerbtn">
+            <IconButton>
+              <MicIcon />
+            </IconButton>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
