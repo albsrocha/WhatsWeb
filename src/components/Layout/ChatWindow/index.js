f3e230da11ca73c6d15b515d83cb49b516d4233d
Alberto Rocha <albsrocha@gmail.com>
2021-09-09 15:33:43 -0300

    Incluido TOP em area de texto

--- src/components/Layout/ChatWindow/index.js
+++ src/components/Layout/ChatWindow/index.js
@@ -1,7 +1,38 @@
 import React from "react";
+import "./index.css";
 
-export default function ChatWindow({text}){
-    return(
-        <div>{text}</div>
-    )
+import SearchIcon from "@material-ui/icons/Search";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import IconButton from "@material-ui/core/IconButton";
+
+export default function ChatWindow({ text }) {
+  return (
+    <div className="chatWindow">
+      <div className="chatWindow--header">
+        <div className="chatWindow--headerinfo">
+          <img
+            className="chatWindow--avatar"
+            src="https://www.blexar.com/avatar.png"
+            alt="teste"
+          />
+          <div className="chatWindow--userinfo">Alberto</div>
+        </div>
+
+        <div className="chatWindow--headerbtn">
+          <IconButton>
+            <SearchIcon />
+          </IconButton>
+
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chatWindow--body">{text}</div>
+
+      <div className="chatWindow--footer"></div>
+    </div>
+  );
 }
